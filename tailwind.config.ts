fd1fe6ef6f2081f21306454dda8b103e6bc99aa1
lalalalala00 <commevvz@naver.com>
2025-10-10 04:38:06 +0900

    modify tail

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,27 @@
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   darkMode: "class",
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//         boxShadow: {
+//           neumorphism: "4px 4px 10px #c5c5c5, -4px -4px 10px #ffffff",
+//           "neumorphism-inset": "inset 4px 4px 10px #c5c5c5, inset -4px -4px 10px #ffffff",
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
 import type { Config } from "tailwindcss";
 
 export default {
@@ -13,10 +37,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        boxShadow: {
-          neumorphism: "4px 4px 10px #c5c5c5, -4px -4px 10px #ffffff",
-          "neumorphism-inset": "inset 4px 4px 10px #c5c5c5, inset -4px -4px 10px #ffffff",
-        },
+      },
+      boxShadow: {
+        neumorphism: "4px 4px 10px #c5c5c5, -4px -4px 10px #ffffff",
+        "neumorphism-inset": "inset 4px 4px 10px #c5c5c5, inset -4px -4px 10px #ffffff",
       },
     },
   },
